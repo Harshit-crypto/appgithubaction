74a0ab2c163661391e1d9d43e7d1871969572d0a
Harshit-crypto <hjain.1523@gmail.com>
2024-10-02 20:03:34 +0530

    unit test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,12 @@
+from src.math_operations import add,subtract
+
+
+def test_add():
+    assert add(1,2) == 3
+    assert add(-1,2) == 1
+
+def test_subtract():
+    assert subtract(1,2) == -1
+    assert subtract(-1,2) == -3
+
+    
